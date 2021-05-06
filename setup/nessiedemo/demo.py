75f17393f47e0301ca010c5a611f806651e871f6
Robert Stupp <snazy@snazy.de>
2021-05-06 17:32:07 +0200

    Python code style and member visiblity and code-docs

--- setup/nessiedemo/demo.py
+++ setup/nessiedemo/demo.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-"""NessieDemo TODO docs."""
+"""NessieDemo handles installing and, if necessary, downloading of dependencies for Nessie Demos."""
 import os
 import signal
 import stat
 import subprocess  # noqa: S404
 import sys
 import time
-from typing import TypeVar, Any
+from typing import Any, TypeVar
 
 import requests
 import yaml
@@ -16,46 +16,44 @@ T = TypeVar("T", bound="NessieDemo")
 
 
 class NessieDemo:
-    """NessieDemo TODO docs."""
+    """NessieDemo is the main setup and dependency handler for Nessie demos."""
 
-    nessie_process_wait_seconds: float = 2.0
+    __nessie_process_wait_seconds: float = 2.0
 
-    versions_yaml: str = None
-    demos_root: str = "https://raw.githubusercontent.com/snazy/nessie-demos/master"
+    __versions_yaml: str
+    __demos_root: str = "https://raw.githubusercontent.com/snazy/nessie-demos/master"
 
-    nessie_api_uri: str = "http://localhost:19120/api/v1"
+    __nessie_api_uri: str = "http://localhost:19120/api/v1"
 
-    native_runner_root: str = "https://github.com/projectnessie/nessie/releases/download"
+    __native_runner_root: str = "https://github.com/projectnessie/nessie/releases/download"
 
-    _versions_dict: dict = None
+    __versions_dict: dict
 
-    _nessie_native_runner: os.path = None
-    _nessie_process: subprocess.Popen = None
+    __nessie_native_runner: str
+    __nessie_process: subprocess.Popen
 
-    _assets_dir: os.path = None
+    __assets_dir: str
 
-    _datasets: dict = dict()
+    __datasets: dict = dict()
 
-    def __init__(self: T, versions_yaml: str, demos_root: str = None) -> None:
-        """Nessie demo TODO docs."""
-        self.versions_yaml = versions_yaml
-        if demos_root:
-            self.demos_root = demos_root
+    def __init__(self: T, versions_yaml: str) -> None:
+        """Takes the name of the versions-dictionary."""
+        self.__versions_yaml = versions_yaml
 
-        versions_url = "{}/configs/{}".format(self.demos_root, self.versions_yaml)
+        versions_url = "{}/configs/{}".format(self.__demos_root, self.__versions_yaml)
 
-        self._assets_dir = (
+        self.__assets_dir = (
             os.path.abspath(os.environ["NESSIE_DEMO_ASSETS"])
             if "NESSIE_DEMO_ASSETS" in os.environ
             else os.path.join(os.getcwd(), "_assets")
         )
 
-        self._versions_dict = yaml.safe_load(_Util.curl(versions_url))
+        self.__versions_dict = yaml.safe_load(_Util.curl(versions_url))
 
     def __str__(self: T) -> str:
-        """Todo the docs."""
+        """String-ified representation."""
         if self.is_nessie_running():
-            run_info = "RUNNING, PID {}".format(self._pid_from_file())
+            run_info = "RUNNING, PID {}".format(self.__pid_from_file())
         else:
             run_info = "not running"
         return "Nessie-Demo: Nessie {nessie_version} ({nessie_running}), Apache Iceberg {iceberg_version}".format(
@@ -65,40 +63,40 @@ class NessieDemo:
         )
 
     def get_nessie_version(self: T) -> str:
-        """Todo the docs."""
-        return self._versions_dict["versions"]["nessie"]
+        """Get the Nessie version defined in the versions-dictionary."""
+        return self.__versions_dict["versions"]["nessie"]
 
     def get_pynessie_version(self: T) -> str:
-        """Todo the docs."""
-        return self._versions_dict["versions"]["pynessie"]
+        """Get the pynessie version defined in the versions-dictionary."""
+        return self.__versions_dict["versions"]["pynessie"]
 
     def get_iceberg_version(self: T) -> str:
-        """Todo the docs."""
-        return self._versions_dict["versions"]["iceberg"]
+        """Get the Iceberg version defined in the versions-dictionary."""
+        return self.__versions_dict["versions"]["iceberg"]
 
-    def _prepare(self: T) -> None:
+    def __prepare(self: T) -> None:
         # Install Python dependencies
-        if "python_dependencies" in self._versions_dict:
-            deps = self._versions_dict["python_dependencies"]
+        if "python_dependencies" in self.__versions_dict:
+            deps = self.__versions_dict["python_dependencies"]
             _Util.exec_fail([sys.executable, "-m", "pip", "install"] + ["{}=={}".format(k, v) for k, v in deps.items()])
 
         # Download nessie native runner binary
-        self._nessie_native_runner = os.path.join(
-            self._assets_dir,
+        self.__nessie_native_runner = os.path.join(
+            self.__assets_dir,
             "nessie-quarkus-{}-runner".format(self.get_nessie_version()),
         )
 
-        if os.path.exists(self._nessie_native_runner) and os.stat(self._nessie_native_runner).st_mode & stat.S_IXUSR == stat.S_IXUSR:
+        if os.path.exists(self.__nessie_native_runner) and os.stat(self.__nessie_native_runner).st_mode & stat.S_IXUSR == stat.S_IXUSR:
             return
 
         nessie_native_runner_url = None
         try:
-            nessie_native_runner_url = self._versions_dict["uris"]["nessie_native_image_binary"]
+            nessie_native_runner_url = self.__versions_dict["uris"]["nessie_native_image_binary"]
         except KeyError:
             pass
         if not nessie_native_runner_url:
             nessie_native_runner_url = "{}/nessie-{}/nessie-quarkus-{}-runner".format(
-                self.native_runner_root,
+                self.__native_runner_root,
                 self.get_nessie_version(),
                 self.get_nessie_version(),
             )
@@ -106,16 +104,16 @@ class NessieDemo:
         # TODO find a way to either download binaries to the same place (don't download AND STORE the same binary again)
         # TODO find a way to remove the downloaded binaries
 
-        _Util.wget(nessie_native_runner_url, self._nessie_native_runner, executable=True)
+        _Util.wget(nessie_native_runner_url, self.__nessie_native_runner, executable=True)
 
-    def _pid_file(self: T) -> os.path:
-        return os.path.join(self._assets_dir, "nessie.pid")
+    def __pid_file(self: T) -> str:
+        return os.path.join(self.__assets_dir, "nessie.pid")
 
-    def _version_file(self: T) -> os.path:
-        return os.path.join(self._assets_dir, "nessie.version")
+    def __version_file(self: T) -> str:
+        return os.path.join(self.__assets_dir, "nessie.version")
 
-    def _pid_from_file(self: T) -> int:
-        pid_file = self._pid_file();
+    def __pid_from_file(self: T) -> int:
+        pid_file = self.__pid_file()
         if not os.path.exists(pid_file):
             return -1
         with open(pid_file, "rb") as inp:
@@ -129,19 +127,23 @@ class NessieDemo:
                 return pid
 
     def is_nessie_running(self: T) -> bool:
-        """Nessie docs TODO."""
-        if self._nessie_process and not self._nessie_process.poll():
+        """Check whether a Nessie process is running."""
+        if hasattr(self, "__nessie_process") and not self.__nessie_process.poll():
             return True
-        return self._pid_from_file() > 0
+        return self.__pid_from_file() > 0
 
     def start(self: T) -> None:
-        """Nessie demo TODO docs."""
-        self._prepare()
+        """Starts the Nessie process.
+
+        A running Nessie process will only be stopped, if it is running a different Nessie version.
+        Necessary Python dependencies will be installed, as defined in the versions-dictionary.
+        """
+        self.__prepare()
 
         if self.is_nessie_running():
             # Nessie process is still alive, leave it running.
-            if os.path.exists(self._version_file()):
-                with open(self._version_file(), "rb") as inp:
+            if os.path.exists(self.__version_file()):
+                with open(self.__version_file(), "rb") as inp:
                     running_version = inp.read().decode("utf-8")
             else:
                 running_version = "UNKNOWN_VERSION"
@@ -159,25 +161,25 @@ class NessieDemo:
         try:
             print("Starting Nessie...")
 
-            self._nessie_process = subprocess.Popen(self._nessie_native_runner, stderr=std_capt, stdout=std_capt)  # noqa: S603
+            self.__nessie_process = subprocess.Popen(self.__nessie_native_runner, stderr=std_capt, stdout=std_capt)  # noqa: S603
 
-            with open(self._pid_file(), "wb") as out:
-                out.write(str(self._nessie_process.pid).encode("utf-8"))
-            with open(self._version_file(), "wb") as out:
+            with open(self.__pid_file(), "wb") as out:
+                out.write(str(self.__nessie_process.pid).encode("utf-8"))
+            with open(self.__version_file(), "wb") as out:
                 out.write(self.get_nessie_version().encode("utf-8"))
 
             try:
                 std_capt.close()
-                self._nessie_process.wait(self.nessie_process_wait_seconds)
+                self.__nessie_process.wait(self.__nessie_process_wait_seconds)
                 with open("nessie-runner-output.log") as log:
                     log_lines = log.readlines()
                 raise Exception(
                     "Nessie process disappeared. Exit-code: {}, stdout/stderr:\n  {}".format(
-                        self._nessie_process.returncode, "  ".join(log_lines)
+                        self.__nessie_process.returncode, "  ".join(log_lines)
                     )
                 )
             except subprocess.TimeoutExpired:
-                print("Nessie running with PID {}".format(self._nessie_process.pid))
+                print("Nessie running with PID {}".format(self.__nessie_process.pid))
                 pass
         except Exception:
             std_capt.close()
@@ -185,15 +187,15 @@ class NessieDemo:
             raise
 
     def stop(self: T) -> None:
-        """Nessie demo TODO docs."""
-        if self._nessie_process:
+        """Stops a running Nessie process. This method is a no-op, if Nessie is not running."""
+        if hasattr(self, "__nessie_process"):
             print("Stopping Nessie ...")
-            exit_code = self._nessie_process.poll()
+            exit_code = self.__nessie_process.poll()
             if not exit_code:
-                self._nessie_process.terminate()
-                self._nessie_process.wait()
+                self.__nessie_process.terminate()
+                self.__nessie_process.wait()
             print("Nessie stopped")
-        pid = self._pid_from_file()
+        pid = self.__pid_from_file()
         if pid > 0:
             timeout_at = time.time() + 10
             while True:
@@ -205,17 +207,25 @@ class NessieDemo:
                     time.sleep(0.1)
                 except OSError:
                     break
-        os.unlink(self._pid_file())
-        os.unlink(self._version_file())
+        os.unlink(self.__pid_file())
+        os.unlink(self.__version_file())
 
     def fetch_dataset(self: T, dataset_name: str) -> dict:  # dict[str, os.path]
-        """Nessie demo TODO docs."""
-        if dataset_name in self._datasets:
-            return self._datasets[dataset_name]
+        """Fetches a data set, a collection of files, for a demo.
 
-        dataset_root = "{}/datasets/{}/".format(self.demos_root, dataset_name)
+        Data sets are identified by a name, which corresponds to a subdirectory underneath the `datasets/`
+        directory in the source tree. If a data set already exists on the executing machine, the data set will
+        not be downloaded again. In other words: it is safe to call this function multiple times for the same data
+        set and it will only be downloaded once.
+        :param dataset_name: the name of the data set to fetch.
+        :return: a dictionary of file-name to path on the running machine.
+        """
+        if dataset_name in self.__datasets:
+            return self.__datasets[dataset_name]
+
+        dataset_root = "{}/datasets/{}/".format(self.__demos_root, dataset_name)
         contents = _Util.curl("{}/ls.txt".format(dataset_root)).decode("utf-8").split("\n")
-        dataset_dir = os.path.join(self._assets_dir, "datasets/{}".format(dataset_name))
+        dataset_dir = os.path.join(self.__assets_dir, "datasets/{}".format(dataset_name))
         if not os.path.isdir(dataset_dir):
             os.makedirs(dataset_dir)
         name_to_path = dict()
@@ -230,9 +240,25 @@ class NessieDemo:
 
         print("Dataset {} with files {}".format(dataset_name, ", ".join(name_to_path.keys())))
 
-        self._datasets[dataset_name] = name_to_path
+        self.__datasets[dataset_name] = name_to_path
 
         return name_to_path
+
+    def _get_versions_dict(self: T) -> dict:
+        """Get the versions-dictionary retrieved from one of the config files in the `configs/` directory."""
+        return self.__versions_dict
+
+    def _get_assets_dir(self: T) -> str:
+        """The directory used to store assets, which are the downloaded Nessie-Quarkus-Runners, Spark tarballs, datasets, etc."""
+        return self.__assets_dir
+
+    def get_nessie_api_uri(self: T) -> str:
+        """Get the Nessie server's API URL."""
+        return self.__nessie_api_uri
+
+    def get_versions_yaml(self: T) -> str:
+        """Get the name of the versions-dictionary, as passed to the constructor of `NessieDemo` or to `nessiedemos.demo.setupDemo()`."""
+        return self.__versions_yaml
 
 
 class _Util:
@@ -241,10 +267,14 @@ class _Util:
         print("Executing {} ...".format(" ".join(args)))
         result = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # noqa: S603
         if result.returncode != 0:
-            raise Exception("Executable failed. args: {}, stdout={}, stderr={}".format(" ".join(result.args), result.stdout, result.stderr))
+            raise Exception(
+                "Executable failed. args: {}, stdout={}, stderr={}".format(
+                    " ".join(result.args), result.stdout.decode("utf-8"), result.stderr.decode("utf-8")
+                )
+            )
 
     @staticmethod
-    def wget(url: str, target: os.path, executable: bool = False) -> None:
+    def wget(url: str, target: str, executable: bool = False) -> None:
         try:
             print("Downloading {} ...".format(url))
             with requests.get(url, stream=True) as resp:
@@ -277,9 +307,18 @@ __NESSIE_DEMO__ = None
 
 
 def setup_demo(versions_yaml: str, datasets: Any) -> NessieDemo:  # datasets: list[str]  or  str
+    """Sets up and starts a `NessieDemo` instance.
+
+    It uses the versions-dictionary identified by `versions_yaml`, fetches the datasets mentioned in the
+    `datasets` parameter and starts a Nessie process.
+    :param versions_yaml: the name of the versions-dictionary, as in the `configs/` directory in the source tree.
+    :param datasets: either a single string or a list of strings with the names of the datasets to fetch.
+    Dictionaries containing the filename-to-path mapping of a dataset can be retrieved by calling `NessieDemo.fetch_dataset`.
+    :return: the constructed `NessieDemo` instance.
+    """
     global __NESSIE_DEMO__
     if __NESSIE_DEMO__:
-        if __NESSIE_DEMO__.versions_yaml == versions_yaml:
+        if __NESSIE_DEMO__.get_versions_yaml() == versions_yaml:
             # reuse the existing NessieDemo instance, if possible
             print("Reusing existing NessieDemo instance: {}".format(__NESSIE_DEMO__))
             return __NESSIE_DEMO__
--- setup/nessiedemo/spark.py
+++ setup/nessiedemo/spark.py
@@ -1,36 +1,53 @@
 # -*- coding: utf-8 -*-
-"""NessieDemo TODO docs."""
+"""NessieDemoSpark handles setting up Spark and Iceberg related objects.
+
+This code shall work for all Spark and Iceberg versions used in all demos.
+Due to that, we cannot add `pyspark` as a dependency into `requirements.txt`, but `NessieDemo` takes care of
+installing the correct `pyspark` version. Since packages like `pyspark` are only available after the dependencies
+have been installed, all Spark related code must be in a separate Python module that is loaded after
+`nessiedemo.demo.setup_demo()` (i.e. `NessieDemo.start()`) has been executed.
+"""
 
 import os
 import re
-from typing import Any, Callable, Tuple, TypeVar
+from typing import Any, Tuple, TypeVar
 
 import findspark  # NOTE: this module is NOT included in requirements.txt
-from .demo import NessieDemo, _Util
 from py4j.java_gateway import java_import  # NOTE: this module is NOT included in requirements.txt
 from pyspark import SparkConf, SparkContext  # NOTE: this module is NOT included in requirements.txt
 from pyspark.sql import SparkSession  # NOTE: this module is NOT included in requirements.txt
+
+from .demo import _Util, NessieDemo
 
 T = TypeVar("T", bound="NessieDemoSpark")
 
 
 class NessieDemoSpark:
-    _demo: NessieDemo = None
-    _assets_dir: os.path = None
+    """`NessieDemoSpark` is a helper class for Spark in Nessie-Demos.
+
+    It contains code that uses pyspark and py4j, which is only available after `NessieDemo` has been prepared (started).
+    """
+
+    __demo: NessieDemo
+    __assets_dir: str
 
     def __init__(self: T, demo: NessieDemo) -> None:
-        self._demo = demo
-        self._assets_dir = demo._assets_dir
+        """Creates a `NessieDemoSpark` instance for respectively using the given `NessieDemo` instance."""
+        self.__demo = demo
+        self.__assets_dir = demo._get_assets_dir()
 
-        spark_url = self._demo._versions_dict["spark"]["tarball"]
+        spark_url = self.__demo._get_versions_dict()["spark"]["tarball"]
         # derive directory name inside the tarball from the URL
-        dir_name = re.match(".*[/]([a-zA-Z0-9-.]+)[.]tgz", spark_url).group(1)
-        spark_dir = os.path.join(self._assets_dir, dir_name)
+        m = re.match(".*[/]([a-zA-Z0-9-.]+)[.]tgz", spark_url)
+        if not m:
+            raise Exception("Invalid Spark download URL {}".format(spark_url))
+        dir_name = m.group(1)
+        spark_dir = os.path.join(self.__assets_dir, dir_name)
         if not os.path.exists(spark_dir):
-            tgz = os.path.join(self._assets_dir, "{}.tgz".format(dir_name))
+            tgz = os.path.join(self.__assets_dir, "{}.tgz".format(dir_name))
             if not os.path.exists(tgz):
                 _Util.wget(spark_url, tgz)
-            _Util.exec_fail(["tar", "-x", "-C", self._assets_dir, "-f", tgz])
+            _Util.exec_fail(["tar", "-x", "-C", self.__assets_dir, "-f", tgz])
 
         print("Using Spark in {}".format(spark_dir))
 
@@ -39,44 +56,47 @@ class NessieDemoSpark:
         findspark.init()
 
     def get_or_create_spark_context(self: T, nessie_ref: str = "main") -> Tuple:  # Tuple[SparkSession, SparkContext, Any]
-        """Nessie demo TODO docs."""
+        """Sets up the `SparkConf`, `SparkSession` and `SparkContext` ready to use for the provided `nessie_ref`.
+
+        :param nessie_ref: the Nessie reference to configure in the `SparkConf`. Can be a branch name, tag name or commit hash.
+        :return: A 3-tuple of `SparkSession`, `SparkContext` and the JVM gateway
+        """
         print("Creating SparkConf, SparkSession, SparkContext ...")
-        conf = SparkConf()
-        self.spark_conf(conf.set, nessie_ref)
+        conf = self.__spark_conf(nessie_ref)
         spark = SparkSession.builder.config(conf=conf).getOrCreate()
         sc = spark.sparkContext
-        jvm = self._jvm_for_iceberg(sc)
+        jvm = self.__jvm_for_iceberg(sc)
         print("Created SparkConf, SparkSession, SparkContext")
 
         return spark, sc, jvm
 
-    def spark_conf(self: T, conf_setter: Callable, nessie_ref: str = "main") -> None:  # conf_setter: Callable[[str, str], Any]
-        """Nessie demo TODO docs."""
-        conf_setter(
+    def __spark_conf(self: T, nessie_ref: str = "main") -> SparkConf:
+        conf = SparkConf()
+        conf.set(
             "spark.jars.packages",
-            "org.apache.iceberg:iceberg-spark3-runtime:{}".format(self._demo.get_iceberg_version()),
+            "org.apache.iceberg:iceberg-spark3-runtime:{}".format(self.__demo.get_iceberg_version()),
         )
-        conf_setter("spark.sql.execution.pyarrow.enabled", "true")
-        conf_setter(
+        conf.set("spark.sql.execution.pyarrow.enabled", "true")
+        conf.set(
             "spark.sql.catalog.nessie.warehouse",
             "file://{}/spark_warehouse".format(os.getcwd()),
         )
-        conf_setter("spark.sql.catalog.nessie.url", self._demo.nessie_api_uri)
-        conf_setter("spark.sql.catalog.nessie.ref", nessie_ref)
-        conf_setter(
+        conf.set("spark.sql.catalog.nessie.url", self.__demo.get_nessie_api_uri())
+        conf.set("spark.sql.catalog.nessie.ref", nessie_ref)
+        conf.set(
             "spark.sql.catalog.nessie.catalog-impl",
             "org.apache.iceberg.nessie.NessieCatalog",
         )
-        conf_setter("spark.sql.catalog.nessie.auth_type", "NONE")
-        conf_setter("spark.sql.catalog.nessie.cache-enabled", "false")
-        conf_setter("spark.sql.catalog.nessie", "org.apache.iceberg.spark.SparkCatalog")
-        conf_setter(
+        conf.set("spark.sql.catalog.nessie.auth_type", "NONE")
+        conf.set("spark.sql.catalog.nessie.cache-enabled", "false")
+        conf.set("spark.sql.catalog.nessie", "org.apache.iceberg.spark.SparkCatalog")
+        conf.set(
             "spark.sql.catalog.spark_catalog",
             "org.apache.iceberg.spark.SparkSessionCatalog",
         )
+        return conf
 
-    def _jvm_for_iceberg(self: T, spark_context: SparkContext) -> Any:
-        """Nessie demo TODO docs."""
+    def __jvm_for_iceberg(self: T, spark_context: SparkContext) -> Any:
         jvm = spark_context._gateway.jvm
 
         java_import(jvm, "org.apache.iceberg.CatalogUtil")
@@ -89,6 +109,11 @@ class NessieDemoSpark:
 
 
 def spark_for_demo(demo: NessieDemo, nessie_ref: str = "main") -> Tuple:  # Tuple[SparkSession, SparkContext, Any, NessieDemoSpark]
+    """Sets up the `SparkConf`, `SparkSession` and `SparkContext` ready to use for the provided `nessie_ref`.
+
+    :param nessie_ref: the Nessie reference to configure in the `SparkConf`. Can be a branch name, tag name or commit hash.
+    :return: A 4-tuple of `SparkSession`, `SparkContext`, the JVM gateway and `NessieDemoSpark`
+    """
     demo_spark = NessieDemoSpark(demo)
     spark, sc, jvm = demo_spark.get_or_create_spark_context(nessie_ref)
     return spark, sc, jvm, demo_spark
